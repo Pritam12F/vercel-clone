c18340016e607dc7e55d05d8f184bb8ca915a61a
PritamDas <pritam.das.santu@gmail.com>
2024-05-29 18:07:25 +0530

    initialized the upload route for github url

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,21 @@ import express from "express";
 const app = express();
 const PORT = 3002;
 
+app.use(express.json());
+
+app.post("/upload", async (req, res) => {
+  const url = req.body?.gitUrl;
+
+  if (url) {
+    return res.json({
+      message: "Caught!",
+    });
+  }
+  return res.json({
+    error: "No url provided",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
