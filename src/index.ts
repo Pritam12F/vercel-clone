d4e66c56b85896d98a74788543f67ed0e1773892
PritamDas <pritam.das.santu@gmail.com>
2024-05-29 18:32:15 +0530

    added generate id func

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3002;
 
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+  })
+);
 app.use(express.json());
+
+const generateId = () => {
+  let str = "";
+  let alpha = "abcdefghijklmnopqrstuvwxyz";
+
+  const random = Math.floor(Math.random() * 100000) + 1;
+
+  for (let i = 0; i < 5; ++i) {
+    const randomAl = alpha[Math.floor(Math.random() * 26)];
+    str += randomAl;
+  }
+
+  str = str.concat(random.toString());
+
+  return str;
+};
 
 app.post("/upload", async (req, res) => {
   const url = req.body?.gitUrl;
 
   if (url) {
     return res.json({
-      message: "Caught!",
+      message: generateId(),
     });
   }
   return res.json({
